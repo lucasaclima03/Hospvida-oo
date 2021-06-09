3b0562b79bcc5f51acab4093d87c455237fcb7c0
lucasaclima03 <79098716+lucasaclima03@users.noreply.github.com>
2021-06-09 17:47:08 -0300

    arquivos index, mapa, css e cadastrar(html feito) criados

---
+++ cadastrar.php
@@ -0,0 +1,36 @@
+<?php
+include 'css.php';
+include 'config.php';
+?>
+<div class = container>
+<h1>Hospitais</h1>
+<br>
+<h2>Cadastrar Novo Hospital</h2>
+<br>
+<form action = "processa_cad.php" method = "POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nome do Hospital</label>
+    <input type="text" class="form-control" name="nome" >    
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Endereço</label>
+    <input type="text" class="form-control" name = "endereco" >
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Latitude</label>
+    <input type="text" class="form-control" name = "lat" >
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Longitude</label>
+    <input type="text" class="form-control" name ="lng">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Observações</label>
+    <input type="text" class="form-control" name = "observacoes">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Cadastrar</button>
+</form>
+
+
+
+</div>
